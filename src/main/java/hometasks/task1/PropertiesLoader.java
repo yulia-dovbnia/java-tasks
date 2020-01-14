2a3b202798f738c80b944b341326baebbbe00c41
Yuliia Dovbnia <yuliadovbnia@gmail.com>
2020-01-14 11:25:53 +0200

    [Reflection and Annotations] Reading properties from target

--- src/main/java/hometasks/task1/PropertiesLoader.java
+++ src/main/java/hometasks/task1/PropertiesLoader.java
@@ -1,6 +1,5 @@
 package hometasks.task1;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -12,7 +11,7 @@ public class PropertiesLoader {
     static Properties properties;
 
     static {
-        try (InputStream input = new FileInputStream("src/main/resources/task1.properties")) {
+        try (InputStream input = PropertiesLoader.class.getClassLoader().getResourceAsStream("task1.properties")) {
             properties = new Properties();
             properties.load(input);
         } catch (IOException ex) {
